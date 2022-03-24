028f14b5d21b252917e016054dd450c913931bc4
Praveen Kumar <pkumar21@paypal.com>
2022-03-24 23:08:58 +0530

    heroku fix

--- chickenkart-api/src/main/java/com/ecommerce/init/ApplicationStarter.java
+++ chickenkart-api/src/main/java/com/ecommerce/init/ApplicationStarter.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
 
 @ComponentScan(basePackages = {"com.ecommerce"})
 @SpringBootApplication
 @EnableAutoConfiguration
+@Controller
 public class ApplicationStarter extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
